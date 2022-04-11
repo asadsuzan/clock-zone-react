6bfa558609a71c1b6be44d28bb5782540ee1880d
suzan <asadsuzan7@gmail.com>
2022-04-11 18:12:44 +0600

    set header and route links

--- src/Componets/Header/Header.js
+++ src/Componets/Header/Header.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div>
-      <h1>this is header</h1>
-    </div>
+    <header className="header bg-light d-flex px-3 shadow-sm align-items-center justify-content-center">
+      <div className="h-brand text-uppercase">
+        <span>clock</span>
+        <span>zone</span>
+      </div>
+      <nav className="h-nav d-flex justify-content-center ">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/shop"}>shop</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/blog"}>blog</Link>
+        <Link to={"/contact"}>contact</Link>
+      </nav>
+      <div className="h-actions-btn d-flex">
+        <AiOutlineUser />
+        <AiOutlineShoppingCart />
+      </div>
+    </header>
   );
 };
 
