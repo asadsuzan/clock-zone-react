cbbd7f9b661ee4b519d2eaeb308a8acac32cb07e
suzan <asadsuzan7@gmail.com>
2022-04-11 17:43:28 +0600

    setup routes and files

---
+++ src/Componets/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>this is footer</p>
+      <code>
+        <small>20222</small>
+      </code>
+    </footer>
+  );
+};
+
+export default Footer;
