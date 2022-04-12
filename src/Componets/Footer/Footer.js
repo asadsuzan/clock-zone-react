e03ebf7bc757f3996a5851a7f0b8f2140291b6d8
suzan <asadsuzan7@gmail.com>
2022-04-12 15:26:39 +0600

    add police at footer

--- src/Componets/Footer/Footer.js
+++ src/Componets/Footer/Footer.js
@@ -1,12 +1,31 @@
 import React from "react";
 
+import { FaPlaceOfWorship } from "react-icons/fa";
+import { AiFillUnlock } from "react-icons/ai";
+import { GrShieldSecurity } from "react-icons/gr";
+import "./Footer.css";
 const Footer = () => {
   return (
-    <footer>
-      <p>this is footer</p>
-      <code>
-        <small>20222</small>
-      </code>
+    <footer className="container">
+      <section className="polices-wraper">
+        <div className="row polices">
+          <div className="col-lg-4 col-md-4 col-12 plc-left">
+            <FaPlaceOfWorship />
+            <h3>Free Shipping Method</h3>
+            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+          </div>
+          <div className="col-lg-4 col-md-4 col-12 plc-middle">
+            <AiFillUnlock />
+            <h3>Secure Payment System</h3>
+            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+          </div>
+          <div className="col-lg-4 col-md-4 col-12 plc-right">
+            <GrShieldSecurity />
+            <h3>Secure Payment System</h3>
+            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+          </div>
+        </div>
+      </section>
     </footer>
   );
 };
